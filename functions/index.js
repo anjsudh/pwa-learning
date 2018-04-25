b449988fad80c9ba35768c79546e49bc002c306c
Anjana S <anjanas@thoughtworks.com>
2018-04-26 03:49:40 +0530

    pwa training code

---
+++ functions/index.js
@@ -0,0 +1,131 @@
+var functions = require("firebase-functions");
+var admin = require("firebase-admin");
+var cors = require("cors")({ origin: true });
+var webpush = require("web-push");
+var fs = require("fs");
+var UUID = require("uuid-v4");
+var os = require("os");
+var Busboy = require("busboy");
+var path = require('path');
+
+var serviceAccount = require("./firebase.json");
+
+var gcconfig = {
+  projectId: "push-notifications-anj",
+  keyFilename: "firebase.json"
+};
+
+var gcs = require("@google-cloud/storage")(gcconfig);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://push-notifications-anj.firebaseio.com/"
+});
+
+exports.storePostData = functions.https.onRequest(function(request, response) {
+  cors(request, response, function() {
+    var uuid = UUID();
+    const busboy = new Busboy({ headers: request.headers });
+    let upload;
+    const fields = {};
+
+    // This callback will be invoked for each file uploaded
+    busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
+      console.log(
+        `File [${fieldname}] filename: ${filename}, encoding: ${encoding}, mimetype: ${mimetype}`
+      );
+      const filepath = path.join(os.tmpdir(), filename);
+      upload = { file: filepath, type: mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+
+    // This will invoked on every field detected
+    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+      fields[fieldname] = val;
+    });
+
+    // This callback will be invoked after all uploaded files are saved.
+    busboy.on("finish", () => {
+      var bucket = gcs.bucket("push-notifications-anj.appspot.com");
+      bucket.upload(
+        upload.file,
+        {
+          uploadType: "media",
+          metadata: {
+            metadata: {
+              contentType: upload.type,
+              firebaseStorageDownloadTokens: uuid
+            }
+          }
+        },
+        function(err, uploadedFile) {
+          if (!err) {
+            admin
+              .database()
+              .ref("posts")
+              .push({
+                title: fields.title,
+                location: fields.location,
+                rawLocation: {
+                  lat: fields.rawLocationLat,
+                  lng: fields.rawLocationLng
+                },
+                image:
+                  "https://firebasestorage.googleapis.com/v0/b/" +
+                  bucket.name +
+                  "/o/" +
+                  encodeURIComponent(uploadedFile.name) +
+                  "?alt=media&token=" +
+                  uuid
+              })
+              .then(function() {
+                webpush.setVapidDetails(
+                  "mailto:anjanas@thoughtworks.com",
+                  "BA3JljQqVycSQuAP8E-TsCwjIexiwHaKcIdCGOwvUgHpgMXEC2c751wK2c4Sj5fa70CF1DZqSk1p30SFBTe6Hg4",
+                  "xu7TTn8klBH_8Kcri2MURMfUaLgfC2vFGA4A--ANolI"
+                );
+                return admin
+                  .database()
+                  .ref("subscriptions")
+                  .once("value");
+              })
+              .then(function(subscriptions) {
+                subscriptions.forEach(function(sub) {
+                  var pushConfig = {
+                    endpoint: sub.val().endpoint,
+                    keys: {
+                      auth: sub.val().keys.auth,
+                      p256dh: sub.val().keys.p256dh
+                    }
+                  };
+
+                  webpush
+                    .sendNotification(
+                      pushConfig,
+                      JSON.stringify({
+                        title: "New Post",
+                        content: "New Post added!",
+                        openUrl: "/help"
+                      })
+                    )
+                    .catch(function(err) {
+                      console.log(err);
+                    });
+                });
+                response
+                  .status(201)
+                  .json({ message: "Data stored", id: fields.id });
+              })
+              .catch(function(err) {
+                response.status(500).json({ error: err });
+              });
+          } else {
+            console.log(err);
+          }
+        }
+      );
+    });
+
+    busboy.end(request.rawBody);
+  });
+});
